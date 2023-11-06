f05df598c7842f1f4a3c63592fd48c5155958322
Saikat Dey <saikatdey2100@gmail.com>
2023-11-06 06:22:57 +0530

    Rotated keys.
    Signed-off-by: Saikat Dey <saikatdey2100@gmail.com>

--- emerald-city-backend/src/Constants.ts
+++ emerald-city-backend/src/Constants.ts
@@ -1,10 +1,10 @@
 //export const MONGO_URI =  `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASSWORD}@cluster0.bavoa.mongodb.net/?retryWrites=true&w=majority`
 //export const MONGO_URI = `mongodb://mongo:123@localhost:27017/`;
-export const MONGO_USER_NAME = 'saikatdey2100';
-export const MONGO_USER_PASSWORD = '0LGOa4J0uhKotzbS';
+export const MONGO_USER_NAME = '';
+export const MONGO_USER_PASSWORD = '';
 export const MONGO_URI = `mongodb+srv://${MONGO_USER_NAME}:${MONGO_USER_PASSWORD}@emerald-cluster.qszztjn.mongodb.net/`;
 export const AWS_S3_BUCKET = 'emerald-city';
 export const AWS_S3_REGION = 'ap-south-1';
 export const DB_NAME = 'emerald';
-export const AWS_ACCESS_KEY_ID = 'AKIA534SVJVTZHDW4AVZ';
-export const AWS_SECRET_ACCESS_KEY = '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';
+export const AWS_ACCESS_KEY_ID = '';
+export const AWS_SECRET_ACCESS_KEY = '';
